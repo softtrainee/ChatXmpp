3c58a2abd8ba503b280c49e7bb136c614b75a5a2
Kshitij Raina <kshitijraina@Kshitijs-iMac.local>
2017-02-22 17:00:10 +0530

    Running Code

---
+++ Pods/Target Support Files/FMDB/FMDB-umbrella.h
@@ -0,0 +1,11 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "FMDatabase.h"
+#import "FMDatabaseAdditions.h"
+#import "FMResultSet.h"
+
+FOUNDATION_EXPORT double FMDBVersionNumber;
+FOUNDATION_EXPORT const unsigned char FMDBVersionString[];
+
---
+++ Pods/Target Support Files/xmpp-messenger-ios/xmpp-messenger-ios-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double xmpp_messenger_iosVersionNumber;
+FOUNDATION_EXPORT const unsigned char xmpp_messenger_iosVersionString[];
+
